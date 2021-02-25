344c9acacb4781b58a9bc8fc77fa67cfae1384ba
VODACOM\makhg002 <given.makhobela@vcontractor.co.za>
2021-02-25 16:12:43 +0200

    Update: Database tables

--- database-table-creation.sql
+++ database-table-creation.sql
@@ -18,75 +18,70 @@ CREATE TABLE user_type (
 );
 
 -- Table to carry user personalm information
-CREATE TABLE user_personal_information (
-	user_personal_information_id INT IDENTITY(1, 1) PRIMARY KEY,
-	fullname NVARCHAR(MAX) NOT NULL,
+CREATE TABLE personal_information (
+	personal_information_id INT IDENTITY(1, 1) PRIMARY KEY,
+	firstname NVARCHAR(MAX) NOT NULL,
+	lastname NVARCHAR(MAX) NOT NULL,
 	phone_number NVARCHAR(MAX) NOT NULL,
 	id_number NVARCHAR(MAX) NOT NULL UNIQUE,
-	user_type INT NOT NULL FOREIGN KEY REFERENCES user_type(user_type_id),
+	user_type_id INT NOT NULL FOREIGN KEY REFERENCES user_type(user_type_id),
 	created_date DATETIME NOT NULL
 );
 
 -- Table to carry teachers infomration
-CREATE TABLE teacher (
+CREATE TABLE teachers (
 	teacher_id INT IDENTITY(1, 1) PRIMARY KEY,
-	teacher_personal_information_id INT NOT NULL FOREIGN KEY REFERENCES user_personal_information(user_personal_information_id)
+	personal_information_id INT NOT NULL FOREIGN KEY REFERENCES personal_information(personal_information_id)
 );
 
 -- Table to carry guardian information
-CREATE TABLE guardian (
-	guardian_id INT IDENTITY(1, 1), PRIMARY KEY,
-	guardian_personal_information INT FOREIGN KEY REFERENCES user_personal_information(user_personal_information_id)
+CREATE TABLE guardians (
+	guardian_id INT IDENTITY(1, 1) PRIMARY KEY,
+	personal_information INT FOREIGN KEY REFERENCES personal_information(personal_information_id)
 );
 
 -- Table to carry grades
-CREATE TABLE grade (
+CREATE TABLE grades (
 	grade_id INT IDENTITY(1, 1) PRIMARY KEY,
-	grade_name NVARCHAR(10) NOT NULL UNIQUE
+	grade NVARCHAR(10) NOT NULL UNIQUE
 );
 
 -- Table to carry subjects
-CREATE TABLE subject (
+CREATE TABLE subjects (
 	subject_id INT IDENTITY(1, 1) PRIMARY KEY,
-	subject_name NVARCHAR(60) NOT NULL UNIQUE
+	subject NVARCHAR(60) NOT NULL UNIQUE,
+	teacher_id INT FOREIGN KEY REFERENCES teachers(teacher_id)
 );
 
 -- Table to carry streams
-CREATE TABLE stream (
+CREATE TABLE streams (
 	stream_id INT IDENTITY(1, 1) PRIMARY KEY,
-	subject_id_1 INT NOT NULL FOREIGN KEY REFERENCES subject(subject_id),
-	subject_id_2 INT NOT NULL FOREIGN KEY REFERENCES subject(subject_id),
-	subject_id_3 INT NOT NULL FOREIGN KEY REFERENCES subject(subject_id),
-	subject_id_4 INT NOT NULL FOREIGN KEY REFERENCES subject(subject_id),
-	subject_id_5 INT NOT NULL FOREIGN KEY REFERENCES subject(subject_id),
-	teacher_id INT NOT NULL FOREIGN KEY REFERENCES teacher(teacher_id),
-	grade_id INT NOT NULL FOREIGN KEY REFERENCES grade(grade_id)
+	subject_id_1 INT NOT NULL FOREIGN KEY REFERENCES subjects(subject_id),
+	subject_id_2 INT NOT NULL FOREIGN KEY REFERENCES subjects(subject_id),
+	subject_id_3 INT NOT NULL FOREIGN KEY REFERENCES subjects(subject_id),
+	subject_id_4 INT NOT NULL FOREIGN KEY REFERENCES subjects(subject_id),
+	subject_id_5 INT NOT NULL FOREIGN KEY REFERENCES subjects(subject_id),
+	grade_id INT NOT NULL FOREIGN KEY REFERENCES grades(grade_id)
 );
 
 -- Table to carry student account status
-CREATE TABLE account_status (
-	account_status_id INT IDENTITY(1, 1) PRIMARY KEY,
-	status_type NVARCHAR(100) NOT NULL UNIQUE
+CREATE TABLE student_status (
+	student_status_id INT IDENTITY(1, 1) PRIMARY KEY,
+	status NVARCHAR(100) NOT NULL UNIQUE
 );
 
-CREATE TABLE student (
+CREATE TABLE students (
 	student_id INT IDENTITY(1, 1) PRIMARY KEY,
-	student_informatio_id INT NOT NULL FOREIGN KEY REFERENCES user_personal_information(user_personal_information_id),
-	stream_id INT NOT NULL FOREIGN KEY REFERENCES stream(stream_id),
-	status_id INT NOT NULL FOREIGN KEY REFERENCES account_status(account_status_id)
+	personal_information INT NOT NULL FOREIGN KEY REFERENCES personal_information(personal_information_id),
+	stream_id INT NOT NULL FOREIGN KEY REFERENCES streamsstream_id),
+	student_status_id INT NOT NULL FOREIGN KEY REFERENCES student_status(student_status_id)
 );
 
 -- Table to carry relationship between parents and students
 CREATE TABLE student_guardian (
 	student_guardian_id INT IDENTITY(1, 1) PRIMARY KEY,
-	guardian_id INT FOREIGN KEY REFERENCES guardian(guardian_id),
-	student_id INT FOREIGN KEY REFEENCES student(student_id)
-);
-
--- Tables to carry Guardian information |Not Done|
-CREATE TABLE guardian (
-        guardian_id INT IDENTITY(1, 1) PRIMARY KEY,
-        guardian_personal_information_id INT NOT NULL FOREIGN KEY REFERENCES user_personal_information(user_personal_information_id)
+	guardian_id INT FOREIGN KEY REFERENCES guardians(guardian_id),
+	student_id INT FOREIGN KEY REFEENCES students(student_id)
 );
 
 ROLLBACK TRANSACTION;
