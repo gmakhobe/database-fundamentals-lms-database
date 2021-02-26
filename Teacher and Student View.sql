886dc13f6fc075076bb8ab133739652bad5b96c3
mmodisad <50734123+mmodisad@users.noreply.github.com>
2021-02-26 07:12:05 +0200

    Update Teacher and Student View.sql

--- Teacher and Student View.sql
+++ Teacher and Student View.sql
@@ -2,21 +2,26 @@ DROP VIEW IF EXISTS student_view;
 GO
 
 CREATE VIEW student_view  AS
-    SELECT [dbo].[Student].StudentID, [dbo].[PersonalInfo].FirstName, 
-	[dbo].[PersonalInfo].LastName,[dbo].[Stream].SubjectID, [dbo].[Teacher].TeacherID
-	,[dbo].[Grade].GradeName
+    SELECT [dbo].[students].student_id, [dbo].[personal_information].firstname, 
+	[dbo].[personal_information].lastname,[dbo].[streams].subject_id_1,
+	[dbo].[streams].subject_id_2, [dbo].[streams].subject_id_3, [dbo].[streams].subject_id_4,
+	[dbo].[streams].subject_id_5, [dbo].[teachers].teacher_id 
+	,[dbo].[grades].grade, [dbo].[student_status].student_status_id, [dbo].[student_status].registered_date, [dbo].[student_status].status
 	FROM
-	[dbo].[Student], [dbo].[PersonalInfo], [dbo].[Stream], [dbo].[Teacher], [dbo].[Grade]
+	[dbo].[students], [dbo].[personal_information], [dbo].[streams], [dbo].[teachers], [dbo].[grades], [dbo].[student_status]
 GO
 
 SELECT * FROM student_view
 GO
 
+
+DROP VIEW IF EXISTS teacher_view;
+GO
+
 CREATE VIEW teacher_view  AS
-    SELECT [dbo].[Teacher].TeacherID, [dbo].[Subject].SubjectName, 
-	[dbo].[Grade].GradeName
+    SELECT [dbo].[teachers].teacher_id, [dbo].[subjects].subject , [dbo].[grades].grade
 	FROM
-	[dbo].[Teacher], [dbo].[Subject],[dbo].[Grade]
+	[dbo].[teachers], [dbo].[subjects],[dbo].[grades]
 GO
 
 SELECT * FROM teacher_view
